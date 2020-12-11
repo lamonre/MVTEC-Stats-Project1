4d02b43a3c600f99e215148bb9066252a8a82c6e
Rebecca Pazos <vivi.pazos@me.com>
2020-12-12 00:38:22 +0800

    trying to group by week

--- P2-RebeccaP-Stats-Clustering.R
+++ P2-RebeccaP-Stats-Clustering.R
@@ -1,1 +1,12 @@
-#Clustering script
+#Clustering script begin
+
+data = read.csv(file = "data_output/rvpazos_preprocessed.csv")
+names(data)
+dim(data)
+summary(data)
+
+#attach(data) didn't work for me
+
+library(cluster)
+
+actives<-c(2:16)
