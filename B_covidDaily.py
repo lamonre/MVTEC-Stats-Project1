f0473a88bd291a58c44443e0b77d1e560af1396f
Rebecca Pazos <vivi.pazos@me.com>
2020-12-19 00:48:28 +0800

    Fixing the URL, testing only for error catching

--- B_covidDaily.py
+++ B_covidDaily.py
@@ -2,7 +2,7 @@ import pandas as pd
 from email_cred import send_mail
 
 
-url = "https://raw.githubusercontent.com/owi2/covid-19-data/master/public/data/owid-covid-data.csv"
+url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
 columns = ['continent', 'location', 'date', 'total_cases', 'total_cases_per_million', 'new_cases_smoothed', 'total_deaths', 'total_deaths_per_million', 'new_deaths_smoothed']
 countries = ["Belgium", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "France", "Germany", "Iceland", "Ireland", "Latvia", "Lithuania", "Malta", "Netherlands", "Norway", "Portugal", "Romania", "Slovenia", "Spain", "United Kingdom", "United States"]
 
